7a29c39dbdae89703743fd4ed817e154a718a739
Rafia Owais <161371012+Rafia1996@users.noreply.github.com>
2024-03-15 20:35:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// object => {} => {key : value} pair
+var student = {
+    // array of object
+    fullname: "Rafia Owais",
+    classid: "abc",
+    rollno: 25,
+    email: "example",
+    ispassed: true,
+    subject: ["english", "urdu", "programming"]
+};
+var student2 = {
+    // array of object
+    fullname: "yahya hira",
+    classid: "abc",
+    rollno: 25,
+    email: "example",
+    ispassed: true,
+    subject: ["english", "urdu", "programming"]
+};
+var students = {
+    // array of object
+    fullname: "rida javed",
+    classid: "abc",
+    rollno: 25,
+    email: "example",
+    ispassed: true,
+    subject: ["english", "urdu", "programming"]
+};
+console.log(student.fullname);
+var arr = [{}, {}, [], [], true, "a"];
+var obj = {
+    key: {
+        key: {}
+    },
+    key2: []
+};
---
+++ index.ts
@@ -0,0 +1,45 @@
+// object => {} => {key : value} pair
+
+let student = {
+
+// array of object
+fullname : "Rafia Owais",
+classid : "abc",
+rollno : 25,
+email: "example",
+ispassed : true,
+subject: [ "english" , "urdu", "programming"]
+};
+
+let student2 = {
+
+    // array of object
+    fullname : "yahya hira",
+    classid : "abc",
+    rollno : 25,
+    email: "example",
+    ispassed : true,
+    subject: [ "english" , "urdu", "programming"]
+    };
+
+    
+    let students = {
+        // array of object
+        fullname : "rida javed",
+        classid : "abc",
+        rollno : 25,
+        email: "example",
+        ispassed : true,
+        subject: [ "english" , "urdu", "programming"]
+        };
+        
+console.log(student.fullname);
+
+
+        let arr = [ {}, {}, [], [], true, "a"]
+        let obj = {
+            key : {
+                key:{}
+            },
+            key2: []
+            }
